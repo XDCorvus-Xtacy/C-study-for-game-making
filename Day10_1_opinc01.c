34bb41f728b974c23e623fbc18b939fdc2614ebc
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-09-24 11:19:51 +0000

    add Day10_1

---
+++ Day10_1_opinc01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+    int x = 10;
+
+    //1. x에 저장된 값과 1을 더해 다시 x에 저장한다.
+    x = x + 1;
+    printf("%d\n", x);
+
+    //2. x에 저장되어 있는 값에 1을 누산한다.
+    x += 1;
+    printf("%d\n", x);
+
+    //3. x에 저장된 값을 1 증가시킨다.
+    ++x;
+    printf("%d\n", x);
+    return 0;
+}
