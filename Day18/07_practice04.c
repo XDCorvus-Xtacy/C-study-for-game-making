18c5e8200ce51aec6bc041baf4ee95a91ca5383f
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-04 12:03:50 +0000

    add Day18_7

---
+++ Day18/07_practice04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 0, j = 0;
+
+    for (i = 0; i <= 5; ++i) {
+        for (j = 1; j <= i; ++j) {
+            printf("* ");
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
