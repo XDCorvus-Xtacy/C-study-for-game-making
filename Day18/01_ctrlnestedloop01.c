1b5e7b4839d1ccfd101f4a17ac32f7320524bbe3
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-04 09:28:54 +0000

    add Day18_1

---
+++ Day18/01_ctrlnestedloop01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    int i = 0, j = 0;
+
+    i = 2;          //계수가 변수 i 초기화
+    while (i <= 9)  //반복 조건
+    {
+        j = 1;
+        while (j <= 9) {
+            printf("%d * %d = %d\n", i, j, i * j);
+            //계수기를 증가시킨다.
+            //따라서 언젠가는 반복이 끝나는 조건에 도달한다.
+            ++j;
+        }
+
+        putchar('\n');
+        ++i;    //계수기 증가
+    }
+
+    return 0;
+}
