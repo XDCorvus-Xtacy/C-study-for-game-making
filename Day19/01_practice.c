094035d5e39cd613e21c076d992fbbeb41bf52b2
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-05 11:10:17 +0000

    Day19_1 add new solution

--- Day19/01_practice.c
+++ Day19/01_practice.c
@@ -20,3 +20,34 @@ int main(void) {
     }
     return 0;
 }
+
+
+
+/*
+창의적인 또다른 방법
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0, j = 0;
+
+    for (i = 0; i < 5; ++i)
+    {
+        for (j = 0; j < 5; ++j)
+        {
+            if (i + j >= 4) printf("*\t");
+            else            printf("\t");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
+
+나는 일일이 반복해줬는데, 총합을 기준으로 계산하는 또다른 방법
+창의적으로 일을 처리할 수 있도록 창의성을 기르는 것이 프로그래밍에서도 정말 중요하다는 걸
+깨닫게 해줌
+*/
