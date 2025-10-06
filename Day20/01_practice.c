bf245e8002b73c83ca7d13ff19a6cbbb468b11d7
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-06 09:18:47 +0000

    Day20_1 add

---
+++ Day20/01_practice.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int cnt = 0, sum = 0, start = 0, end = 0;
+    char buffer[100];
+
+    printf("숫자 범위 입력(띄어쓰기로 구분 ex. 10 20, 1 100 ...) : ");
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, "%d %d", &start, &end);
+
+    for (int i = start; i <= end; i++)
+    {
+        int remainder = i%4;
+
+        if (remainder == 0)
+        {
+            sum += i;
+            cnt += 1;
+            printf("i : %d, cnt : %d, sum : %d\n", i, cnt, sum);
+        }
+    }
+
+    printf("\n\n");
+    printf("개수 : %d, 총합 : %d\n", cnt, sum);
+    
+    return 0;
+}
