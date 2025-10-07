c0010c66f2dd5244f955b7a562faa7c85c9828ed
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-07 03:49:15 +0000

    Day21_5 add

---
+++ Day21/05_practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int aList[5] = {30, 40, 10, 50, 20};
+    int i = 0, nTmp = 0;
+
+    for (i=1; i<5; ++i)
+    {
+        if (aList[0] > aList[i])
+        {
+            aList[0] = aList[i];
+            nTmp = i;
+        }
+    }
+
+    aList[nTmp] = 30;
+
+    for (i=0; i<5; ++i)
+        printf("%d\t", aList[i]);
+    putchar('\n');
+
+    printf("MIN: %d\n", aList[0]);
+    return 0;
+}
