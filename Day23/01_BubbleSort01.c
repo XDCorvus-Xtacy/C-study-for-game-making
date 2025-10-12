7c7b06ec94a59e4cbd4fb68df2707e7c42319e66
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-12 12:02:14 +0000

    Day23_1 fix efficiently

--- Day23/01_BubbleSort01.c
+++ Day23/01_BubbleSort01.c
@@ -5,7 +5,7 @@ int main(void)
     int aList[5] = {30, 40, 10, 50, 20};
     int i = 0, j = 0, nTmp = 0;
 
-    for (i=0; i<5; i++)
+    for (i=0; i<4; i++)
     {
         for (j=i+1; j<5; j++)
         {
