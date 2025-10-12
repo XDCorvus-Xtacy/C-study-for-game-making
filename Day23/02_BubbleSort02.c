2e91b057a73f99c4937f5a85c642b1e43a05a8a7
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-10-12 11:35:35 +0000

    Day23_2 add

---
+++ Day23/02_BubbleSort02.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int aList[5] = {30, 40, 10, 50, 20};
+    int i = 0, j = 0, nTmp = 0;
+
+    for (i=0; i<5; i++)
+    {
+        for (j=0; j<5-i; j++)
+        {
+            if (aList[j] > aList[j+1])
+            {
+                nTmp = aList[j];
+                aList[j] = aList[j+1];
+                aList[j+1] = nTmp;
+            }
+        }
+    }
+
+    for (i=0; i<5; ++i)
+    {
+        printf("%d\t", aList[i]);
+    }
+    putchar('\n');
+    return 0;
+}
