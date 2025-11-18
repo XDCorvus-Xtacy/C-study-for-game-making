f5b7d0e7b49ad09eb64c9172552b28528d62c298
XDCorvus-Xtacy <doxa0430@gmail.com>
2025-11-18 11:58:35 +0000

    refactor: change (Day34 <-> Day35)

--- Day34/01_ptrstrcmp1.c
+++ Day34/01_ptrstrcmp1.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
-    char *pszBuffer = NULL;
+    char szBuffer[12] = {"TestString"};
+    char *pszData = "TestString";
     
-    pszBuffer = (char*)malloc(12);
-    printf("입력: ");
-    //gets(pszBuffer);  //오버플로우를 방지하기 위해 gets_s(), fgets() 사용할 것
-    fgets(pszBuffer, 12, stdin);
-    puts(pszBuffer);
-    
-    //오류 확인을 위해 의도적으로 해제하지 않음
-    free(pszBuffer);  //free를 동작하게 될 경우 오류 메세지 발생
+    //다음 코드들은 두 문자열이 같은지 비교하는 것이 아니라
+    //문자열이 저장된 메모리의 위치가 같은지 비교하는 것이다.
+    printf("%d\n", szBuffer == pszData);
+    printf("%d\n", "TestString" == pszData);
+    printf("%d\n", "DataSString" == "TestString");
     return 0;
 }
 
+
+/*
+질문
+
+1. 위 printf()에서 ==는 무슨 연산을 하고 뭐를 반환하지?
+2. 위 printf는 주소 비교 결과를 프린트하는 것인데,
+   왜 주소가 같은 경우도 존재하는 거지?
+*/
