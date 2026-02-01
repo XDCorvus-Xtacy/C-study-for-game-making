4dba0f5c442eaecd18013faca5596c51960b5cef
XDcorvus-Xtacy <doxa0430@gmail.com>
2026-02-01 16:20:38 +0900

    Day54_8 add explain

--- Day54/08_Filefseek01.c
+++ Day54/08_Filefseek01.c
@@ -7,7 +7,9 @@ void main()
     fp = fopen("Test.dat", "rb");
 
     //파일 입출력 포인터를 파일의 끝으로 이동시킨다.
-    fseek(fp, 0, SEEK_END);
+    fseek(fp, 0, SEEK_END);     //SEEK_END: 끝
+                                //SEEK_SET: 처음
+                                //SEEK_CUR: 현재
     //파일 포인터의 현재 위치를 출력한다.
     //앞서 파일 입출력 포인터를 맨 끝으로 옮겼으므로 파일 크기를 알 수 있다.
     printf("size of Test.dat : %u\n", ftell(fp));
