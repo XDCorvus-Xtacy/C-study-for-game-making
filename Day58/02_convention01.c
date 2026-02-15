4bf713c7e08162a0ae3de8c0dfe4aed42a47dcb7
XDcorvus-Xtacy <doxa0430@gmail.com>
2026-02-15 19:01:15 +0900

    Day58_2 add explain

--- Day58/02_convention01.c
+++ Day58/02_convention01.c
@@ -1,3 +1,23 @@
+/*
+함수 호출 규칙
+
+1. __cdecl 
+    매개변수 스택 정리: Caller
+    매개변수 메모리: Stack
+
+2. __stdcall 
+    매개변수 스택 정리: Caller
+    매개변수 메모리: Stack
+
+3. __fastcall
+    매개변수 스택 정리: Callee
+    매개변수 메모리: Stack + Reqister
+
+
+우리가 자동변수를 선언할 떄 auto를 생략해도 되는 것처럼 __cdecl도 생략 가능
+원래의 경우는 아래와 같이 기술할 것
+*/
+
 #include <stdio.h>
 
 int __cdecl main(void)
